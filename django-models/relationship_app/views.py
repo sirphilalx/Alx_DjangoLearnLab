10d5c58fc952fb48e263535f4d3299a92151af6a
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-08-20 09:54:50 +0100

    implemented the register, login and logout modules of django

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,6 +4,9 @@ from .models import Book
 from .models import Library
 from .forms import CreateUserForm, LoginForm
 
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
 # authentication models and functions
 from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate, login, logout
