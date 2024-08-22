3688813ee1764461693676d0fc7f312b6dad10bf
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-08-22 06:39:44 +0100

    more changes

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -16,4 +16,9 @@ urlpatterns = [
     path('admin/', admin, name='admin'),
     path('librarian/', librarian, name='librarian'),
     path('member/', member, name='member'),
+
+    # new path for book management
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 ]
