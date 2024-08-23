16007a545dbca9a8f40c008e8aad7e6cab0c8f39
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-08-23 23:57:44 +0100

    changes

--- advanced_features_and_security/relationship_app/admin.py
+++ advanced_features_and_security/relationship_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Author, Book, Library, Librarian
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('name',)
@@ -13,7 +15,15 @@ class LibraryAdmin(admin.ModelAdmin):
 class LibrarianAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Book, BookAdmin)
 admin.site.register(Library, LibraryAdmin)
 admin.site.register(Librarian, LibrarianAdmin)
+admin.site.register(User, CustomUserAdmin)
--- advanced_features_and_security/relationship_app/models.py
+++ advanced_features_and_security/relationship_app/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
+from django.contrib.auth.models import BaseUserManager
+
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -57,6 +59,33 @@ class UserProfile(models.Model):
     def __str__(self):
         return f"{self.user.username} - {self.role}"
     
+
+
+
+
+# base user manager
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
 # custom authentication methods
 # todo:
 # 1. Override the email authentication
@@ -69,6 +98,8 @@ class User(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
+    objects = UserManager()
+    
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
