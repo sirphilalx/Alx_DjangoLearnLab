a0bf5de4b1cd21866bf40bdb47b1e80ffe64d95d
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-08-24 07:58:10 +0100

    groups permission

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,9 +2,10 @@
 from django import forms
 from .models import Book
 
-        
-
-
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author']
 
 
 class BookForm(forms.ModelForm):
