7c9c9e88c1f856c6dd780096af4d7d35a6bb1ee5
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-08-24 01:37:48 +0100

    adding the relationship app

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
-    path('add/', add_book, name='add_book'),
-    path('edit/<int:book_id>/', edit_book, name='edit_book'),
-    path('delete/<int:book_id>/', delete_book, name='delete_book'),
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', delete_book, name='delete_book'),
 ]
