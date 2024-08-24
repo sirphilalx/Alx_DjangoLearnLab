e1e5e1317162a35e1205e15157dd76fe20f2d688
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-08-24 01:35:03 +0100

    adding the relationship app

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,7 +4,7 @@ from .views import list_books, LibraryDetailView
 # views for authentications
 from django.contrib.auth import views as auth_views
 from . import views 
-from .views import admin_view, librarian_view, member_view
+from .views import admin_view, librarian_view, member_view, add_book, edit_book, delete_book
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
@@ -16,4 +16,7 @@ urlpatterns = [
     path('admin-view/', admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
+    path('add/', add_book, name='add_book'),
+    path('edit/<int:book_id>/', edit_book, name='edit_book'),
+    path('delete/<int:book_id>/', delete_book, name='delete_book'),
 ]
