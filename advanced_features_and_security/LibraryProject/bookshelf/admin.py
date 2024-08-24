6ffbe1811de7687367f380ce9db47f0faa0c1bd4
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-08-24 02:00:22 +0100

    update

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,11 @@
 # myapp/admin.py
 from django.contrib import admin
 from .models import Book
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django import forms
+from .models import CustomUser
 
 class BookAdmin(admin.ModelAdmin):
     # Fields to display in the list view
@@ -15,7 +20,43 @@ class BookAdmin(admin.ModelAdmin):
     # Customize the form for adding/editing
     fields = ('title', 'author', 'publication_year')
     
+
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('email', 'username', 'date_of_birth', 'profile_photo')
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
+        fields = ('email', 'username', 'date_of_birth', 'profile_photo')
+
+class CustomUserAdmin(BaseUserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    list_display = ('email', 'username', 'date_of_birth', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Personal info', {'fields': ('date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'date_of_birth', 'profile_photo', 'password1', 'password2', 'is_staff', 'is_active')
+        }),
+    )
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+
    
 
 # Register the Book model with the custom admin options
 admin.site.register(Book, BookAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
