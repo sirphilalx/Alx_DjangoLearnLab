98e4673ad76bdb802d4d75f10e1f68c369a085b5
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-08-29 06:45:20 +0100

    initial commit

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Book
 
-class BookSerializer(serializers.modelSerializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['title', 'author']
