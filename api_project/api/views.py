061486d520e9da821f5bd2bdcc5a152ee2d7bad6
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-08-29 06:46:20 +0100

    initial commit

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,6 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookListCreateAPIView(generics.ListCreateAPIView):
+class BookListCreateAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
