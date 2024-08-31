7e434b73fb6c2682c9d0558df8c2b2062ea840fb
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-08-31 07:55:41 +0100

    viewsets and routers

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from .views import BookListAPIView
+from django.urls import path, include
+from .views import BookViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'book', BookViewSet)
 
 urlpatterns = [
-    path('api/books', BookListAPIView.as_view(), name='book_list_create'),
+    path('api/books', include(router.urls)),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,10 @@
 
-from rest_framework import generics
+from rest_framework import  viewsets
 from .models import Book
 from .serializers import BookSerializer
 
-class BookListAPIView(generics.ListAPIView):
+
+
+class BookViewSet(viewsets.BookViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
