f3f7971dac6e5f4bbf4d591d6e5d04dbc8a36a9e
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-09-02 11:37:55 +0100

    hot fix

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,7 @@
 from rest_framework import  viewsets
 from .models import Book
 from .serializers import BookSerializer
-from .views import BookList
+# from .views import BookList
 # generics.ListAPIView
 
 
