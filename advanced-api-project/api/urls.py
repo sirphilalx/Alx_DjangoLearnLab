2f3bd222d8fe99f669dc5bbf13ef8eca9f4ad35f
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-09-05 10:04:29 +0100

    authentication

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,6 +5,8 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
     path('books/create/', views.BookCreateView.as_view(), name='book-create'),
+    path('books/update/', views.BookCreateView.as_view(), name='book-create'),
+    path('books/delete/', views.BookCreateView.as_view(), name='book-create'),
     path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='book-update'),
     path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book-delete'),
 ]
