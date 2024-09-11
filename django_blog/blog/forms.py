b8eb09617586b11eae230b885c4916ed5d0a3633
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-09-11 12:20:44 +0100

    templates added

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
+from .models import Post
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -29,3 +30,19 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['bio']
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+        
+    def __init__(self, *args, **kwargs):
+        self.author = kwargs.pop('author', None)
+        super(PostForm, self).__init__(*args, **kwargs)
+        
+    def clean(self):
+        cleaned_data = super().clean()
+        if self.author:
+            cleaned_data['author'] = self.author
+        return cleaned_data
--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('post', PostListView.as_view(), name='post_list'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
