f0634ca40152188131b7fdd44baf8a1db2f53885
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-09-11 11:01:43 +0100

    templates added

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -12,4 +12,10 @@ class Post(models.Model):
         return self.title
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    # profile_picture = models.ImageField(upload_to='profile_pics', blank=True)
 
+    def __str__(self):
+        return f'{self.user.username} Profile'
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm
+from .models import Post
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'profile.html')
+
+
+def home(request):
+    return render(request, 'home.html')
+
+@login_required
+def posts(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'posts.html', {'posts': posts})
